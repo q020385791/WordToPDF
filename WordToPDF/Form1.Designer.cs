cd161ee7c96eff8f0af2e40f5ab69f727b8ce0e8
sai wanza <q020385791@gmail.com>
2021-03-02 09:08:31 +0800

    加入專案檔案。

---
+++ WordToPDF/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace WordToPDF
+{
+    partial class WordToPDF
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetFileSource = new System.Windows.Forms.Button();
+            this.btnClearFileSource = new System.Windows.Forms.Button();
+            this.labFileTarget = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "轉換";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "檔案來源：";
+            // 
+            // btnGetFileSource
+            // 
+            this.btnGetFileSource.Location = new System.Drawing.Point(12, 46);
+            this.btnGetFileSource.Name = "btnGetFileSource";
+            this.btnGetFileSource.Size = new System.Drawing.Size(129, 32);
+            this.btnGetFileSource.TabIndex = 3;
+            this.btnGetFileSource.Text = "選取檔案來源";
+            this.btnGetFileSource.UseVisualStyleBackColor = true;
+            this.btnGetFileSource.Click += new System.EventHandler(this.btnGetFileSource_Click);
+            // 
+            // btnClearFileSource
+            // 
+            this.btnClearFileSource.Location = new System.Drawing.Point(147, 46);
+            this.btnClearFileSource.Name = "btnClearFileSource";
+            this.btnClearFileSource.Size = new System.Drawing.Size(92, 32);
+            this.btnClearFileSource.TabIndex = 4;
+            this.btnClearFileSource.Text = "重新選擇";
+            this.btnClearFileSource.UseVisualStyleBackColor = true;
+            this.btnClearFileSource.Click += new System.EventHandler(this.btnClearFileSource_Click);
+            // 
+            // labFileTarget
+            // 
+            this.labFileTarget.AutoSize = true;
+            this.labFileTarget.Location = new System.Drawing.Point(11, 103);
+            this.labFileTarget.Name = "labFileTarget";
+            this.labFileTarget.Size = new System.Drawing.Size(146, 18);
+            this.labFileTarget.TabIndex = 5;
+            this.labFileTarget.Text = "請選取檔案來源...";
+            // 
+            // WordToPDF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 241);
+            this.Controls.Add(this.labFileTarget);
+            this.Controls.Add(this.btnClearFileSource);
+            this.Controls.Add(this.btnGetFileSource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "WordToPDF";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGetFileSource;
+        private System.Windows.Forms.Button btnClearFileSource;
+        private System.Windows.Forms.Label labFileTarget;
+    }
+}
+
---
+++ WordToPDF/Form1.cs
@@ -0,0 +1,85 @@
+﻿using Spire.Doc;
+using Spire.Doc.Documents;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordToPDF
+{
+    public partial class WordToPDF : Form
+    {
+        public WordToPDF()
+        {
+            InitializeComponent();
+        }
+        public string FileSpurcePath = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IfLabTargetFileNull())
+            {
+                MessageBox.Show("請選取檔案來源...");
+                return;
+            }
+            else
+            {
+                try
+                {
+                    Document doc = new Document(FileSpurcePath);
+                    doc.SaveToFile(System.IO.Path.GetFileName(FileSpurcePath).Split('.')[0].ToString()+".PDF", FileFormat.PDF);
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("唔!出錯了，請檢查該檔案是否為.doc、.docx檔案");
+                }
+            }
+
+
+        }
+
+        private void btnGetFileSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "選檔案唷ww";
+            dialog.InitialDirectory = ".\\";
+            //篩選 為doc docx檔案
+            dialog.Filter = "doc files (*.*)|*.docx;*.doc;";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                FileSpurcePath = dialog.FileName;
+                labFileTarget.Text = dialog.FileName;
+            }
+            else
+            {
+                labFileTarget.Text = "請選取檔案來源...";
+            }
+        }
+
+        public bool IfLabTargetFileNull()
+        {
+            if (string.IsNullOrEmpty(labFileTarget.Text) || labFileTarget.Text == "請選取檔案來源...")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+        }
+
+        private void btnClearFileSource_Click(object sender, EventArgs e)
+        {
+            FileSpurcePath = "";
+            labFileTarget.Text = "請選取檔案來源...";
+        }
+    }
+}
